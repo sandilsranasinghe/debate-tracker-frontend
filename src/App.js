bff19f6b9b90d1e8ee0fcb1082ec7e851103112c
Dineth Hettiarachchi <dinethhettiarachchi5@gmail.com>
2024-10-19 16:41:22 +0530

    add debater rankings

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Menu from './components/Menu.js'; // Ensure this path is correct
 import DebaterList from './components/DebaterList.js';
+import Rankings from "./components/Rankings.js";
 // Import other components as needed
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
                 <Menu />
                 <Routes>
                     <Route path="/debaters" element={<DebaterList />} />
+                    <Route path="/rankings" element={<Rankings />} />
                     {/*<Route path="/judges" element={<JudgesComponent />} />*/}
                     {/*<Route path="/rankings" element={<RankingsComponent />} />*/}
                     {/*<Route path="/tournaments" element={<TournamentsComponent />} />*/}
